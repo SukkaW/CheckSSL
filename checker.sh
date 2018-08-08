973fca23b756db4a0d6e1617230b157e789632fa
SukkaW <isukkaw@gmail.com>
2018-08-08 13:44:30 +0800

    refactor/ci: change output directories

--- checker.sh
+++ checker.sh
@@ -1,29 +1,30 @@
 chmod +x runcheck.sh
 
+mkdir ./tmp/api -p
+
 for i in $@
 do
     ./runcheck.sh ${i}
 done
 
-
-mkdir tmp/api -p
-
-echo '[' > tmp/api/ct.json
+echo '[' > ./tmp/api/ct.json
 
 for i in $@
 do
-    cat tmp/${i}.json >> tmp/api/ct.json
+    cat ./tmp/${i}.json >> ./tmp/api/ct.json
 done
 
-sed -i '$d' tmp/api/ct.json
+sed -i '$d' ./tmp/api/ct.json
 
-echo '}' >> tmp/api/ct.json
-echo ']' >> tmp/api/ct.json
-sed -i ':label;N;s/\n/ /;b label' tmp/api/ct.json
-sed -i "s|\" \"||g" tmp/api/ct.json
-sed -i "s|\"\: \"|\"\:\"|g" tmp/api/ct.json
-sed -i "s|\"\, \"|\"\,\"|g" tmp/api/ct.json
-sed -i "s|\" }, { \"|\"},{\"|g" tmp/api/ct.json
+echo '}' >> ./tmp/api/ct.json
+echo ']' >> ./tmp/api/ct.json
 
-mkdir public/api -p
+sed -i ':label;N;s/\n/ /;b label' ./tmp/api/ct.json
+
+sed -i "s|\" \"||g" ./tmp/api/ct.json
+sed -i "s|\"\: \"|\"\:\"|g" ./tmp/api/ct.json
+sed -i "s|\"\, \"|\"\,\"|g" ./tmp/api/ct.json
+sed -i "s|\" }, { \"|\"},{\"|g" ./tmp/api/ct.json
+
+mkdir public -p
 cp -rf tmp/api/ct.json public/ct.json
