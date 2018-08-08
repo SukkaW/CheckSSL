8ac2c2bd3622d34a1688c36fc259bd2a6ee5c0fe
SukkaW <isukkaw@gmail.com>
2018-08-08 15:08:34 +0800

    feat(runcheck): improve status output

--- runcheck.sh
+++ runcheck.sh
@@ -1,15 +1,14 @@
-curl https://${1} -v -s -o /dev/null 2> ./tmp/ca.info
+curl https://${1} -k -v -s -o /dev/null 2> ./tmp/ca.info
 
 cat ./tmp/ca.info | grep 'start date: ' > ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'expire date: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'issuer: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
 
-sed -i 's|\* start date: ||g' ./tmp/api/ct.json
-sed -i 's|\* expire date: ||g' ./tmp/api/ct.json
-sed -i 's|\* issuer: ||g' ./tmp/api/ct.json
-sed -i 's|\* SSL certificate verify ||g' ./tmp/api/ct.json
-sed -i 's|ok.|ok|g' ./tmp/api/ct.json
+sed -i 's|* start date: ||g' ./tmp/${1}.info
+sed -i 's|* expire date: ||g' ./tmp/${1}.info
+sed -i 's|* issuer: ||g' ./tmp/${1}.info
+sed -i 's|* SSL certificate verify ||g' ./tmp/${1}.info
 
 start=$(sed -n '1p' ./tmp/${1}.info)
 expire=$(sed -n '2p' ./tmp/${1}.info)
@@ -32,7 +31,21 @@ echo '"domain": "'${1}'",' >> ./tmp/${1}.json
 echo '"start": "'$start'",' >> ./tmp/${1}.json
 echo '"expire": "'$expire'",' >> ./tmp/${1}.json
 echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
-echo '"status": "'$status'",' >> ./tmp/${1}.json
+
+if [ $expirestamp < $nowstamp ]; then
+    echo '"status": "Expired",' >> ./tmp/${1}.json
+    echo '"statuscolor": "error",' >> ./tmp/${1}.json
+elif [ $expireday \< 10 ]; then
+    echo '"status": "Soon Expired",' >> ./tmp/${1}.json
+    echo '"statuscolor": "warning",' >> ./tmp/${1}.json
+elif [ $status = "ok." ]; then
+    echo '"status": "Valid",' >> ./tmp/${1}.json
+    echo '"statuscolor": "success",' >> ./tmp/${1}.json
+else
+    echo '"status": "Invalid",' >> ./tmp/${1}.json
+    echo '"statuscolor": "error",' >> ./tmp/${1}.json
+fi
+
 echo '"check": "'$DATE'"' >> ./tmp/${1}.json
 echo '"remain": "'$expireday'"' >> ./tmp/${1}.json
 echo '},' >> ./tmp/${1}.json
