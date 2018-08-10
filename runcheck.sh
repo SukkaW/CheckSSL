c169ec767929cfc5d7b6d652e848cb4823197211
SukkaW <isukkaw@gmail.com>
2018-08-10 14:07:03 +0800

    feat: get subject info

--- runcheck.sh
+++ runcheck.sh
@@ -9,16 +9,19 @@ cat ./tmp/ca.info | grep 'start date: ' > ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'expire date: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'issuer: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
+cat ./tmp/ca.info | grep 'subject: ' >> ./tmp/${1}.info
 
 sed -i 's|\* \t start date: ||g' ./tmp/${1}.info
 sed -i 's|\* \t expire date: ||g' ./tmp/${1}.info
 sed -i 's|\* \t issuer: ||g' ./tmp/${1}.info
 sed -i 's|\* \t SSL certificate verify ||g' ./tmp/${1}.info
+sed -i 's|\* \t subject: ||g' ./tmp/${1}.info
 
 start=$(sed -n '1p' ./tmp/${1}.info)
 expire=$(sed -n '2p' ./tmp/${1}.info)
 issuer=$(sed -n '3p' ./tmp/${1}.info)
 status=$(sed -n '4p' ./tmp/${1}.info)
+subject=$(sed -n '5p' ./tmp/${1}.info)
 
 rm -f ./tmp/ca.info
 rm -f ./tmp/${1}.info
@@ -31,6 +34,7 @@ expireday=`expr $[expirestamp-nowstamp] / 86400`
 
 echo '{' > tmp/${1}.json
 echo '"domain": "'${1}'",' >> ./tmp/${1}.json
+echo '"subject": "'$subjec'",' >> ./tmp/${1}.json
 echo '"start": "'$start'",' >> ./tmp/${1}.json
 echo '"expire": "'$expire'",' >> ./tmp/${1}.json
 echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
