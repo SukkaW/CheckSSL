be466602856bfd00a8387803306356eca6a892ef
SukkaW <isukkaw@gmail.com>
2018-08-10 16:18:49 +0800

    merge
    * refactor: remove useless file
    
    * fix: directories
    
    * fix: remove debug message
    
    * ci: output debug information during build
    
    * ci: add back travis.yml
    
    * feat: get subject info
    
    * feat: output subject in json
    
    * fix: variable name error
    
    * refactor/ci: got what i needed, disable debug info output

--- runcheck.sh
+++ runcheck.sh
@@ -1,28 +1,27 @@
+mkdir ./tmp -p
+
 curl https://${1} -k -v -s -o /dev/null 2> ./tmp/ca.info
 
 cat ./tmp/ca.info | grep 'start date: ' > ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'expire date: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'issuer: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
-
-cat ./tmp/${1}.info
+cat ./tmp/ca.info | grep 'subject: ' >> ./tmp/${1}.info
 
 sed -i 's|\* \t start date: ||g' ./tmp/${1}.info
 sed -i 's|\* \t expire date: ||g' ./tmp/${1}.info
 sed -i 's|\* \t issuer: ||g' ./tmp/${1}.info
 sed -i 's|\* \t SSL certificate verify ||g' ./tmp/${1}.info
-
-cat ./tmp/${1}.info
+sed -i 's|\* \t subject: ||g' ./tmp/${1}.info
 
 start=$(sed -n '1p' ./tmp/${1}.info)
 expire=$(sed -n '2p' ./tmp/${1}.info)
 issuer=$(sed -n '3p' ./tmp/${1}.info)
 status=$(sed -n '4p' ./tmp/${1}.info)
+subject=$(sed -n '5p' ./tmp/${1}.info)
 
-rm -f tmp/ca.info
-rm -f tmp/${1}.info
-
-mkdir tmp -p
+rm -f ./tmp/ca.info
+rm -f ./tmp/${1}.info
 
 DATE="$(echo $(date '+%Y-%m-%d %H:%M:%S'))"
 
@@ -32,6 +31,7 @@ expireday=`expr $[expirestamp-nowstamp] / 86400`
 
 echo '{' > tmp/${1}.json
 echo '"domain": "'${1}'",' >> ./tmp/${1}.json
+echo '"subject": "'$subject'",' >> ./tmp/${1}.json
 echo '"start": "'$start'",' >> ./tmp/${1}.json
 echo '"expire": "'$expire'",' >> ./tmp/${1}.json
 echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
